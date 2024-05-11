9dfc4acac31ad8772ace65275b75832fea42549b
TopD <dagz55@gmail.com>
2024-05-12 02:42:49 +0800

    download only

---
+++ recs-armory/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run any necessary startup commands or services
+# For example:
+# service1 start
+# service2 start
+
+# Run the Istio installation script
+/usr/local/bin/install-istio.sh
+
+# Start the main process or command for the container
+# For example:
+# exec "$@"
